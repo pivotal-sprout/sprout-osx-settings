b35bdfff59e75e1060f78286b8ef3cce2cca00af
Abhijit Hiremagalur <pair+abhi@pivotallabs.com>
2013-04-19 18:12:37 -0700

    Fix accidental circular cookbook dependency

--- metadata.rb
+++ metadata.rb
@@ -6,4 +6,4 @@ description      'Recipes to configure various aspects of OS X'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 supports         'mac_os_x'
-depends          'pivotal_workstation'
+depends          'osx'
