a6fa98039dd572c890ff310666e5bbc838fd9385
Abhi Hiremagalur <abhijit@hiremaga.com>
2014-04-19 22:21:00 -0700

    sprout-osx-base was renamed to sprout-base and there is no defualt recipe in this cookbook

--- metadata.rb
+++ metadata.rb
@@ -6,6 +6,6 @@ description      'Recipes to configure various aspects of OS X'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 supports         'mac_os_x'
-depends          'sprout-osx-base'
+depends          'sprout-base'
 depends          'homebrew'
 depends          'osx'
