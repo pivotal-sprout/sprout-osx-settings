57481d1db89320b862744077ac3e9b450788f83a
Abhi Hiremagalur <abhijit@hiremaga.com>
2014-04-20 10:25:22 -0700

    Move screen_sharing_* from pivotal_workstation

---
+++ recipes/screen_sharing.rb
@@ -0,0 +1,31 @@
+ruby_block "Copy Screen Sharing to /Applications" do
+  block do
+    FileUtils.cp_r("/System/Library/CoreServices/Screen\ Sharing.app","/Applications/", preserve: true)
+  end
+  not_if do
+    File.exists?("/Applications/Screen Sharing.app")
+  end
+end
+
+execute "Enabling Screen Sharing for All Users" do
+  command "/System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -activate -configure -access -off -restart -agent -privs -all -allowAccessFor -allUsers"
+end
+
+ruby_block "assure that /Applications/Screen Sharing.app was successfully created" do
+  block do
+    raise "Copying /Applications/Screen Sharing.app to /System/Library/CoreServices/Screen Sharing.app failed." unless File.exists?("/Applications/Screen Sharing.app")
+  end
+end
+
+ruby_block "test to see if screen-sharing is enabled" do
+  block do
+    require 'socket'
+    vnc_port = 5900
+    begin
+      s = TCPSocket.open('localhost',vnc_port)
+    rescue => e
+      raise "Couldn't connect to screen-sharing: " << e
+    end
+    s.close
+  end
+end
